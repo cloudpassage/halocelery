da7b24465016814d2577dcdfe23bed58c807a8d5
Ash Wilson <ash.d.wilson@gmail.com>
2017-01-06 17:26:26 -0800

    chg: dev: Fixed issue where we weren't getting the right depth into the json to get events !minor

--- apputils/halo.py
+++ apputils/halo.py
@@ -134,7 +134,7 @@ class Halo(object):
         search_params = {"server_id": server_id,
                          "sort_by": "created_at.desc",
                          "since": starting}
-        halo_events = h_h.get("/v1/events", params=search_params)
+        halo_events = h_h.get("/v1/events", params=search_params)["events"]
         for event in halo_events:
             if len(events) >= number_of_events:
                 return events
